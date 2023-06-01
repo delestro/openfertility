d932cbfc3a81a7773aa89e16301f6b0ab5f8a41d
Felipe Delestro Matos <fima@pop-os.localdomain>
2023-06-01 19:29:05 +0200

    Checks if data already downloaded

--- openfertility/datasets/blasto2k.py
+++ openfertility/datasets/blasto2k.py
@@ -1,7 +1,9 @@
+"Dataset class for the blasto2k dataset"
+
+import os
+import outputformat as ouf
 from openfertility.datasets import utils
 from openfertility.io.logger import log
-import outputformat as ouf
-import os
 
 
 class Dataset:
@@ -9,27 +11,37 @@ class Dataset:
     name = "blasto2k"
     reference = "https://doi.org/10.1038/s41597-023-02182-3"
 
-    def download(self, force=False):
-        # Download dataset
-        extract_path = os.path.join(os.getcwd(), self.name)
+    def __init__(self, download=False, **kwargs):
+        self.dataset_path = os.path.join(os.getcwd(), self.name)
+        self.name_bold = ouf.b(self.name, return_str=True)
+        if download:
+            self.download(**kwargs)
 
-        if os.path.exists(extract_path) and not force:
-            log.info(f"{ouf.b(self.name, return_str=True)} already at {extract_path}")
-            log.info('Use force=True to download again')
+        if not os.path.exists(self.dataset_path):
+            log.info("Dataset not found at: %s", self.dataset_path)
+            log.info("Use the method '.download()' from the created instance")
+
+    def download(self, force=False, **kwargs):
+        # Download dataset
+
+        if os.path.exists(self.dataset_path) and not force:
+            log.info("%s already at %s", self.name_bold, self.dataset_path)
+
+            log.info("Use force=True to download again")
             return
-        else:
-            log.info(f"Downloading {ouf.b(self.name, return_str=True)}  {self.reference}")
-            utils.download_file(self.url, f"{self.name}.zip")
+
+        # Proceed with download if dataset not found
+        log.info("Downloading %s - %s", self.name_bold, self.reference)
+        utils.download_file(self.url, f"{self.name}.zip")
 
         # Unzip dataset
         if not os.path.exists(self.name):
             os.makedirs(self.name)
 
-        log.info(f"Extracting to {extract_path}")
-        utils.extract_zip(f"{self.name}.zip", extract_path)
+        log.info("Extracting to %s", self.dataset_path)
+        utils.extract_zip(f"{self.name}.zip", self.dataset_path)
 
         # Remove zip file
-        log.info(f"Cleaning...")
+        log.info("Cleaning...")
         os.remove(self.name + ".zip")
-        log.info(f"Done!")
-
+        log.info("Done!")
