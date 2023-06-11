c5f633f4f4dac12692b8a8a5b55267abe8a17e32
Felipe Delestro <delestro@gmail.com>
2023-06-11 22:20:48 +0200

    splits info from dataset class

--- openfertility/datasets/blasto2k.py
+++ openfertility/datasets/blasto2k.py
@@ -11,17 +11,7 @@ class Dataset:
     name = "blasto2k"
     reference = "https://doi.org/10.1038/s41597-023-02182-3"
 
-    def __init__(self, download=False, **kwargs):
-        self.dataset_path = os.path.join(os.getcwd(), self.name)
-        self.name_bold = ouf.b(self.name, return_str=True)
-        if download:
-            self.download(**kwargs)
-
-        if not os.path.exists(self.dataset_path):
-            log.info("Dataset not found at: %s", self.dataset_path)
-            log.info("Use the method '.download()' from the created instance")
-
-    def download(self, force=False, **kwargs):
+    def download(self, force=False):
         # Download dataset
 
         if os.path.exists(self.dataset_path) and not force:
@@ -29,10 +19,9 @@ class Dataset:
 
             log.info("Use force=True to download again")
             return
-
-        # Proceed with download if dataset not found
-        log.info("Downloading %s - %s", self.name_bold, self.reference)
-        utils.download_file(self.url, f"{self.name}.zip")
+        else:
+            log.info(f"Downloading {ouf.b(self.name, return_str=True)}  {self.reference}")
+            utils.download_file(self.url, f"{self.name}.zip")
 
         # Unzip dataset
         if not os.path.exists(self.name):
--- openfertility/datasets/splits.py
+++ openfertility/datasets/splits.py
@@ -9,22 +9,23 @@ from PIL import Image
 
 
 class TrainSet(torch.utils.data.Dataset):
-    def __init__(self, dataset, target="EXP_silver", transform=ToTensor()):
-        self.target = target
+    def __init__(self, dataset, transform=ToTensor()):
+        self.dataset = dataset
+        self.target = dataset.target  # Target label
         self.transform = transform
 
         # Directory for images
-        self.image_dir = os.path.join(dataset.name, "Images")
+        self.image_dir = os.path.join(self.dataset.name, self.dataset.data_path)
 
         # Get the labels dataframe
-        self.labels_path = os.path.join(dataset.name, "Gardner_train_silver.csv")
-        self.df = pd.read_csv(self.labels_path, sep=";")
+        self.labels_path = os.path.join(self.dataset.name, self.dataset.labels_path)
+        self.df = pd.read_csv(self.labels_path, sep=dataset.separator)
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.df["Image"][idx])
+        img_path = os.path.join(self.image_dir, self.df[self.dataset.data_col][idx])
         image = Image.open(img_path).convert("RGB")
 
         label = self.df[self.target][idx]
